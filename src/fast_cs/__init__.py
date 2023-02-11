6a2c8262beefb37754a7672e703c702cfef18685
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2023-02-11 21:00:17 +0000

    Add initial fast-cs Controller

--- src/fast_cs/__init__.py
+++ src/fast_cs/__init__.py
@@ -1,6 +1,10 @@
 from importlib.metadata import version
 
+from pydantic import BaseSettings
+
+from .controller import Controller
+
 __version__ = version("fast-cs")
 del version
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "Controller", "BaseSettings"]
